0f335dafa2d8fa7becd50901ddd62b90a5e764bb
Vincent Passama <vincent.passama@capgemini.com>
2015-09-16 10:46:01 +0200

    Adding optional callback function in collection script. Adding translations.

--- Resources/public/js/collection.js
+++ Resources/public/js/collection.js
@@ -22,20 +22,28 @@ define(['jquery'], function($) {
         });
     };
 
-    var init = function() {
-        var collectionHolder = $('.form-with-collection #collection');
-
-        $('.add-item-collection-btn').on('click', function() {
+    var addEvent = function(container, collectionHolder, callbackFunction) {
+        $(container).on('click', function() {
             var prototype = collectionHolder.attr('data-prototype');
             var number = parseInt($('#collection .item:last-child').data('number')) + 1;
             var newSubForm = prototype.replace(/__name__/g, number);
             $('#collection').append(newSubForm);
             $('#collection .item:last-child').data('number', number);
             deleteEvent('#collection .item:last-child .delete-item-collection-btn');
-        });
 
+            if (callbackFunction)
+                callbackFunction();
+        });
+    };
+
+    var initCollection = function(callbackFunction) {
+        if (callbackFunction)
+            callbackFunction();
+
+        var collectionHolder = $('.form-with-collection #collection');
+        addEvent('.add-item-collection-btn', collectionHolder, callbackFunction);
         deleteEvent('.delete-item-collection-btn');
     };
 
-    return init;
+    return initCollection;
 });
