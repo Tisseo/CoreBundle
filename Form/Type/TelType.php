a8c926764f222d96500e14403e7732cd1b91d384
Pierre-Yves Claitte <pierre.cl@gmail.com>
2016-02-04 10:55:28 +0100

    adds 3 customs form fields (email, tel, password)

---
+++ Form/Type/TelType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tisseo\CoreBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TelType extends AbstractType
+{
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'widget' => 'single_text'
+            )
+        );
+    }
+    public function getParent()
+    {
+        return 'text';
+    }
+
+    public function getName()
+    {
+        return 'tisseo_tel';
+    }
+}
