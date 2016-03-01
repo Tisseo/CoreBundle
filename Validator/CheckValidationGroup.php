f8176c8ca8cba0a480dfb03ddc3500e8ad7f1576
Vincent Passama <astek.vpassama@tisseo.fr>
2016-03-01 10:01:21 +0100

    add a static method in order to determine the validation group of an entity, looking at its ID.

---
+++ Validator/CheckValidationGroup.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tisseo\CoreBundle\Validator;
+
+use Symfony\Component\Form\FormInterface;
+
+class CheckValidationGroup
+{
+    /**
+     * Resolve validation group looking at the entity id value
+     * If the entity is 'new', return [Default,Registration] else
+     * return [Default,Edition]
+     *
+     * @param FormInterface $form
+     * @return array
+     */
+    public static function resolve(FormInterface $form)
+    {
+        if ($form->getData()->getId() !== null) {
+            return array ('Default', 'Edition');
+        }
+
+        return array('Default', 'Registration');
+    }
+}
