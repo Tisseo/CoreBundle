6e87e30cef3ae644121f5aa8e18540594c159397
Vincent Passama <astek.vpassama@tisseo.fr>
2016-04-04 10:15:36 +0200

    delete-confirm is replaced by confirm

--- Resources/public/js/confirm.js
+++ Resources/public/js/confirm.js
@@ -1,9 +1,9 @@
 define(['jquery', 'bootbox'], function($, bootbox) {
     /*
-     * Managing the bootbox popup on a delete link.
+     * Managing the bootbox popup on a .confirm link.
      *
      * Expected HTML structure to make this script work
-     * <a href='' class='delete-confirm' data-message=''></a>
+     * <a href='' class='confirm' data-message=''></a>
      */
     var init = function (l) {
         var locale = l || 'fr';
@@ -11,7 +11,7 @@ define(['jquery', 'bootbox'], function($, bootbox) {
             locale: locale
         });
 
-        $(document).on("click", ".delete-confirm", function(event) {
+        $(document).on("click", ".confirm", function(event) {
             event.preventDefault();
             var self = this;
             bootbox.confirm($(this).data('message'), function(result) {
