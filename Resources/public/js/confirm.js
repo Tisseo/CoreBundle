0649d1d959edfefb9f547a81b7a3f864ca768cd9
Vincent Passama <astek.vpassama@tisseo.fr>
2016-05-17 11:12:10 +0200

    add locale into globale requirejs config

--- Resources/public/js/confirm.js
+++ Resources/public/js/confirm.js
@@ -5,12 +5,12 @@ define(['jquery', 'bootbox'], function($, bootbox) {
      * Expected HTML structure to make this script work
      * <a href='' class='confirm' data-message=''></a>
      */
-    var init = function (l, callback) {
-        var locale = l || 'fr';
-        bootbox.setDefaults({
-            locale: locale
-        });
 
+    bootbox.setDefaults({
+        locale: requirejs.s.contexts._.config.config.i18n.locale
+    });
+
+    var init = function (callback) {
         $(document).on("click", ".confirm", function(event) {
             event.preventDefault();
             var self = this;
